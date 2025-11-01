d2a4735664ecb654fd7f04a19b5cf694e275f142
Амантур Эркинов <amanturerkinov@Noutbuk-Amantur.local>
2025-11-01 17:37:09 +0600

    *2

--- project/settings.py
+++ project/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-4kjpdeec9i-er9y2122by@!x0@(_x8*5yvzw^x#ewtca404_ne
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = ["ethnicgames.eu", "www.ethnicgames.eu"]
+# 
 
 # Application definition
 
